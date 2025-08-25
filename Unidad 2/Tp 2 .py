bb82b989e5a32576ddad3c2cb540bc882a0e653a
Camilo-Angeleri <ceangeleri8@gmail.com>
2025-08-25 18:06:31 -0300

    Tp 3

---
+++ Unidad 2/Tp 2 .py
@@ -0,0 +1,16 @@
+# UTN-TUPaDProgramacion1
+# Trabajo practico Nº 2
+
+# Ejercicio Nº1
+Edad = int(input("Ingrese su Edad: ")) 
+if Edad > 18 :  
+    print("Eres mayor de Edad")
+
+# Ejercicio N°2
+nota = int(input("Ingrese su nota:"))
+if nota >= 6:
+    print("Aprobado")
+else:
+    print("Desaprobado")
+
+# Ejercicio
