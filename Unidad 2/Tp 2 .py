a7bb648b312d6ac92108396d3ef6fd4a055eedec
Camilo-Angeleri <ceangeleri8@gmail.com>
2025-08-26 14:55:33 -0300

    Tp 2 hasta ejercicio N°6

--- Unidad 2/Tp 2 .py
+++ Unidad 2/Tp 2 .py
@@ -13,4 +13,56 @@ if nota >= 6:
 else:
     print("Desaprobado")
 
-# Ejercicio
+# Ejercicio N°3
+num1= int(input("Ingrese un numero par "))
+ParoImpar = (num1 % 2)
+if ParoImpar == 0:
+    print("Ha ingresado un numero Par")
+else:
+    print("Por favor, Ingrese un numero Par")
+
+# Ejercicio N°4
+edad = int(input("Ingrese su edad"))
+if edad < 12:
+    print("Usted es un/a Niño/a")
+elif edad >= 12 and edad < 18 :
+    print("Usted es un/a Adolescente")
+elif edad >= 18 and edad < 30 :
+    print("Usted es un/a Adulto/a joven")
+elif edad >= 30 :
+    print("Usted es un/a Adulto/a")
+
+# Ejercicio N°5
+contraseña = input("Ingrese un contraseña de entre 8 y 14 caracteres")
+longtext = len(contraseña)
+if longtext >= 8 and longtext <= 14:
+    print("Ha ingresado una contraseña correcta")
+else:
+    print("Por favor, Ingrese una contraseña de entre 8 y 14 caracteres")
+
+# Ejercicio N°6
+import random
+nums = [random . randint(1, 100) for i in range(50)]
+from statistics import mode, median, mean
+lista = (nums)
+mediaN = mean(lista)
+ModoN = mode(lista)
+MedianaN = median(lista)
+print(lista)
+print(mediaN)
+print(MedianaN)
+print(ModoN)
+if mediaN > MedianaN and MedianaN > ModoN:
+    print("Hay Sesgo positivo o a la derecha")
+elif mediaN < MedianaN and MedianaN< ModoN:
+    print("Hay Sesgo negativo o a la izquierda")
+elif mediaN == MedianaN and MedianaN == ModoN and ModoN == mediaN:
+    print("Sin Sesgo")
+
+# Ejercicio N°7
+
+# Ejercicio N°8
+
+# Ejercicio N°9
+
+# Ejercicio N°10
