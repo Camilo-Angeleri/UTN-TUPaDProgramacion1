78e2e5c698a7e0880320a1bcb4a73fff1ca8c0f5
Camilo-Angeleri <ceangeleri8@gmail.com>
2025-09-09 18:19:34 -0300

    unidad 4

---
+++ Unidad 4/Tp 3 .py
@@ -0,0 +1,40 @@
+# Ejercicio Nº1
+for i in range(101):
+    print(i)
+
+# Ejercicio Nº2
+
+
+# Ejercicio Nº3
+num1 = int(input("Ingrese un numero entero"))
+num2 = int(input("Ingrese un numero entero"))
+sumatoria = 0
+
+if num1 > num2 :
+    for i in range((num2)+1 ,num1):
+     sumatoria += i 
+    print(f"la sumatoria de los numeros intermedios es ",sumatoria)
+else:
+    for i in range((num1)+1,num2):
+     sumatoria += i
+    print(f"la sumatoria de los numeros intermedios es",sumatoria)
+
+# Ejercicio Nº4
+
+
+# Ejercicio Nº5
+
+
+# Ejercicio Nº6
+
+
+# Ejercicio Nº7
+
+
+# Ejercicio Nº8
+
+
+# Ejercicio Nº9
+
+
+# Ejercicio Nº10
